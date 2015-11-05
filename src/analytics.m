d60fdf17eaf47f42ccfbefb8ee81a8d828783e9c
Geir Kulia <geir@kulia.no>
2015-11-05 22:21:28 +0100

    Added simuntanious plots

--- src/analytics.m
+++ src/analytics.m
@@ -34,13 +34,16 @@ function analytics()
 	thd_f = total_harmonic_distortion(V_f);
 	disp(['Total Harmonic Distortion: ' num2str(20 * log10(thd_f)) ' dBc'])
 	disp(['Total Harmonic Distortion: ' num2str((100 * thd_f)) ' %'])
-	disp(['Total Harmonic Distortion: ' num2str(thd(s)) ' dBc'])
+	disp(['Total Harmonic Distortion: ' num2st(rthd(s, Fs, 1000)) ' dBc'])
 
 	saveas(fig, '../fig/log_6_freq_spec.png', 'png')
 	saveas(fig, '../fig/log_6_freq_spec.eps', 'epsc')
 
+	V_f_offgrid = V_f;
+	f_offgrid = f;
+
 	%--------------------------- ONGRID ------------------------
-	clearvars -except fig
+	clearvars -except fig V_f_offgrid f_offgrid
 	ongrid = load('../mat/log4_voltage.mat');
 	s = ongrid.log4.data;
 
@@ -70,7 +73,27 @@ function analytics()
 	thd_f = total_harmonic_distortion(V_f);
 	disp(['Total Harmonic Distortion: ' num2str(20 * log10(thd_f)) ' dBc'])
 	disp(['Total Harmonic Distortion: ' num2str((100 * thd_f)) ' %'])
-	disp(['Total Harmonic Distortion: ' num2str(thd(s)) ' dBc'])
+	disp(['Total Harmonic Distortion: ' num2str(thd(s, Fs, 1000)) ' dBc'])
 
 	saveas(fig, '../fig/log_4_freq_spec.png', 'png')
 	saveas(fig, '../fig/log_4_freq_spec.eps', 'epsc')
+
+
+	%------------------- Plot both ------------------------
+
+	V_f_offgrid = 20* log10(V_f_offgrid ./ max(V_f_offgrid));
+	V_f = 20* log10(V_f ./ max(V_f));
+
+	fig = fig + 1;
+	figure(fig)
+	clf(fig)
+
+	hold on
+	plot(f, V_f, 'r')
+	plot(f_offgrid, V_f_offgrid, 'b')
+	axis([0 Fs/2 min(V_f(end/2:end)) max(V_f)+2])
+	title('Spectrum analysis of on and offgrid')
+	xlabel('Frequency [Hz]')
+	ylabel('Relative amplitude [dB]')
+	legend('Ongrid', 'Offgrid')
+	hold off
